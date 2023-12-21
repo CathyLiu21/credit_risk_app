0e3b8fba12bf0494bd70ce502a3bcb5433419e7c
CathyLiu21 <144642319+CathyLiu21@users.noreply.github.com>
2023-12-20 23:04:54 -0500

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -1,5 +1,25 @@
 import streamlit as st
+import numpy as np
+import pickle
 
-st.title('🎈 App Name')
+st.title("Credit Risk Prediction")
 
-st.write('Hello world!')
+age = st.slider("Select Age", 18, 70, 30)
+sex = st.radio("Select Gender", ["Male", "Female"])
+job = st.slider("Select Job Type", 0, 3, 1)
+housing = st.radio("Owns a House?", ["Yes", "No"])
+saving_account = st.selectbox("Saving Account Status", ["little", "moderate", "quite rich", "rich"])
+checking_amount = st.slider("Checking Amount", 0, 20000, 1000)
+credit_amount = st.slider("Credit Amount", 0, 50000, 10000)
+duration = st.slider("Duration (Months)", 0, 72, 12)
+purpose = st.selectbox("Purpose", ["radio/tv", "education", "furniture/equipment", "new car", "used car", "business", "domestic appliance", "repairs", "other"])
+
+if st.button("Predict"):
+    test_arr = np.array([age, sex, job, housing, saving_account, checking_amount, credit_amount, duration, purpose])
+    test_arr = np.reshape(test_arr, (1, -1))
+
+    model = pickle.load(open('ml_model.pkl', 'rb'))
+    prediction = model.predict(test_arr)
+    predicted = "Risky" if prediction else "No Risk" 
+    result = f"The model has predicted that the result is: {predicted}"
+    st.write(result)
